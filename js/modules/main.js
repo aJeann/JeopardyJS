2d53f0dd704f09f7c2b66dabc7b7b97608d0c028
aJeann <axel.jeansson@hotmail.se>
2021-05-04 00:21:05 +0200

    test

--- js/modules/main.js
+++ js/modules/main.js
@@ -1,11 +1,9 @@
 import Header from "./header.js";
 import Footer from "./footer.js";
 import PlayerSection from "./playersection.js";
-import PlayerModal from "./playermodal.js";
 
 window.addEventListener("load", async (e) => {
     new Header("header");
     new PlayerSection(".player-section");
     new Footer("footer");
-    new PlayerModal(".player-modal");
 });
